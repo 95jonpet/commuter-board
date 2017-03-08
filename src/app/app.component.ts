9356d6a802077a465d220f371c05d8f20130b3bc
Peter Jonsson <95jonpet@gmail.com>
2017-03-09 00:17:25 +0100

    Interface mockup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    template: `
+        <div class="pure-g">
+            <div class="pure-u-1-3">
+                <div class="panel">
+                    <div class="panel--header">
+                        <i class="fa fa-fw fa-info-circle"></i> Panel
+                    </div>
+                    <div class="panel--contents">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                </div>
+            </div>
+        </div>
+    `,
 })
 export class AppComponent  { name = 'Angular'; }
