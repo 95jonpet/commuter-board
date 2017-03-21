cef7ce3ecbcfa2d9649a4df80adc6067e5c93ca4
Peter Jonsson <95jonpet@gmail.com>
2017-03-22 00:25:14 +0100

    Fixed structure fail

--- src/app/add-card.component.ts
+++ src/app/add-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Departure } from './departure';
+import { Departure } from './classes/departure';
 
 @Component({
     selector: 'add-card',
