21f5d702a08b3dab693afccec46521ded90c6fbe
amanske <alexanderjanmanske@gmail.com>
2017-03-22 23:05:44 +0100

    Some design

--- src/app/add-card.component.ts
+++ src/app/add-card.component.ts
@@ -19,7 +19,29 @@ import { Departure } from './classes/departure';
 	                	</div>
                 	</div>
                 	<div class="panel--contents">
-                	hejhejehj
+                		<form class="pure-form">
+                			<fieldset>
+                				<legend>Add new station</legend>
+
+                				<div class="pure-g">
+                					<div class="pure-u-14-24">
+		                				<input type="station" name="Station">
+		                			</div>
+		                			<div class="pure-u-10-24">
+		        						
+								        <input id="train" class="checkbox-custom" type="checkbox" checked>
+								        <label for="train" class="checkbox-custom-label"><i class="fa fa-train"></i> Train</label>
+								        <input id="subway" class="checkbox-custom" type="checkbox" checked>
+								        <label for="subway" class="checkbox-custom-label"><i class="fa fa-subway"></i> Subway</label>
+								        <input id="bus" class="checkbox-custom" type="checkbox" checked>
+								        <label for="bus" class="checkbox-custom-label"><i class="fa fa-bus"></i> Bus</label>    
+								        <input id="boat" class="checkbox-custom" type="checkbox">
+								        <label for="boat" class="checkbox-custom-label"><i class="fa fa-ship"></i> Boat</label>    
+								    </div>
+								   
+							    </div>
+                			</fieldset>
+                		</form>
                 	</div>
                 </div>
             </div>
@@ -72,6 +94,44 @@ import { Departure } from './classes/departure';
             text-decoration: none;
             cursor: pointer;
         }
+
+        .panel--header, .panel--contents {
+        	padding-left: 40px;
+        	padding-right: 20px;
+        }
+
+		.checkbox-custom {
+		    opacity: 0;
+		    position: absolute;   
+		}
+
+		.checkbox-custom, .checkbox-custom-label {
+		    display: inline-block;
+		    vertical-align: middle;
+		    margin: 5px;
+		    cursor: pointer;
+		}
+
+		.checkbox-custom-label {
+		    position: relative;
+		}
+
+		.checkbox-custom + .checkbox-custom-label:before {
+		    content: '';
+		    background: #fff;
+		    border: 2px solid #ddd;
+		    display: inline-block;
+		    vertical-align: middle;
+		    width: 20px;
+		    height: 20px;
+		    padding: 2px;
+		    margin-right: 10px;
+		    text-align: center;
+		}
+
+		.checkbox-custom:checked + .checkbox-custom-label:before {
+		    background-color: #007991;
+		}
     `]
 })
 export class AddCardComponent {
