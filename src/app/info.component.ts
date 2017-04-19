05c8cfbefadc5ef64353bffaf3382bdeee141f5b
Fredrik Liljedahl <fredrik.liljedahl@hotmail.com>
2017-04-19 15:17:32 +0200

    infoinfo

--- src/app/info.component.ts
+++ src/app/info.component.ts
@@ -19,7 +19,23 @@ import { Departure } from './classes/departure';
 	                	</div>
                 	</div>
                 	<div class="panel--contents">
-                        {{informationText}}                        
+                        <h4>What is Commuter Board?</h4>
+                        <p>Commuter Board is a digital message board containing information about SL's
+                        commuting information tailored by you. The board contains cards of two sorts. Single station
+                        card is a card connected to a single commuting end point, for example a subway station or bus stop. 
+                        This card then shows times of departures close to the present from that end point with continuously 
+                        updating information. The other type of card is a trip card where you specify the start and destination
+                        endpoints. This card will show trips from start to destination using only SL's end points. Trips are a collection
+                        of departures connecting start to destination such that every departure can be taken in one trip</p>
+
+                        <h4>How do I use Commuter Board?</h4>
+                        <p>Using the Commuter Board is simple. To add a card you press the + button in the upper right corner of the screen.
+                        This opens a panel from which you can choose either if you want to create a staion card or a trip card. Text
+                        fields with responsive auto completion makes it easy for you to enter the correct end point. Uncheck the boxes
+                        if there is a way of travel you don't want included in the card. Then click 'Add new card' and a card is added
+                        to your very own Commuter Board. These cards are saved locally on your computer. If you want to remove or change 
+                        color of card you press the pen icon in the upper right corner and options to remove or change color will appear
+                        on the card. Click the pen icon again to hide these options.</p>
                 	</div>
                 </div>
             </div>
